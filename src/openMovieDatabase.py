805f7eb77478974e90f90ee6d6cea3929837f26d
Marcus Mu <chunkhang@gmail.com>
2017-02-25 18:46:40 +0800

    Added search function

--- src/openMovieDatabase.py
+++ src/openMovieDatabase.py
@@ -4,7 +4,6 @@ from sys import exit
 from console import *
 from bs4 import BeautifulSoup
 from collections import OrderedDict
-from pprint import pprint
 
 def main():
 
@@ -25,12 +24,16 @@ def main():
 	while True:
 		print()
 		printHeader('Home Menu')
-		options = ['Instant', 'Search', 'Exit']
+		options = ['Quick Hit', 'Search', 'Exit']
 		choice = getChoice([0, 1, 2], options)
 		printDivider(PROGRAM_WIDTH)
 
 		# Prepare request
-		payload = {'tomatoes': 'true' if ROTTEN_TOMATOES else 'false', 'plot': 'full' if FULL_PLOT else 'short'}
+		payload = {
+			'type': 'movie',
+			'tomatoes': 'true' if ROTTEN_TOMATOES else 'false', 
+			'plot': 'full' if FULL_PLOT else 'short'
+		}
 		if choice == 0 or choice == 1:
 			print()
 			printHeader(options[choice], newLineBelow=False)
@@ -42,92 +45,119 @@ def main():
 				if response == 'N':
 					correctedQuery = query
 			if choice == 0:
-				payload['t'] = correctedQuery
+				searching = False
 			else:
-				payload['s'] = correctedQuery
+				searching = True
 		else:
 			exit(0)
 
 		# Send request
-		while True:
-			print('\nSending query to database...')
-			try:
-				r = requests.get(API_URL, params=payload)
-				break
-			except requests.exceptions.ConnectionError:
-				print('Cannot establish connection to database.\n')
-				hitEnter('retry')
+		print('\nSending query to database...')
+		res = sendRequest(correctedQuery, API_URL, payload, search=searching)
+		printDivider(PROGRAM_WIDTH)
 
-		# Parse response
-		if r.status_code == 200:
-			print('Receiving information from database...')
-			printDivider(PROGRAM_WIDTH)
-			res = r.json()
-
-			# Only allow movies
-			if 'Type' in res.keys() and res['Type'] != 'movie':
-				res['Response'] = 'False'
-				res['Error'] = 'Only movies are allowed!'
-
-			if res['Response'] == 'True':
-				title = res['Title']
-				year = res['Year']
-				information = [
-					# Basic information
-					OrderedDict([
-						('plot', res['Plot']),
-						('director', res['Director']),
-						('writer', res['Writer']),
-						('actors', res['Actors'])
-					])
-				]
-				if ROTTEN_TOMATOES:
-					information.append(
-						# Rotten Tomatoes information
-						OrderedDict([
-							('consensus', res['tomatoConsensus']),
-							('tomatometer', res['tomatoMeter'] + '%'),
-							('average rating', res['tomatoRating'] + '/10'),
-							('url', res['tomatoURL'])
-						])
-					)
-				if EXTRA_INFO:
-					information.append(
-						# Extra information	
-						OrderedDict([
-							('genre', res['Genre']),	
-							('rated', res['Rated']),
-							('runtime', res['Runtime']),
-							('release date', res['Released'])
-						])
-					)
-
+		# Parse response			
+		if res['Response'] == 'True':	
+			# Quick hit
+			if not searching:
 				# Display information
-				print()
-				printHeader('%s (%s)' % (title, year))
-				for info in information:
-					for key in info.keys():
-						print(key.upper() + '\n')
-						detail = info[key]
-						if 'N/A' in detail:
-							detail = '-'
-						print(leftPad(leftAlign(detail, PROGRAM_WIDTH-3), 3))
-						print()
-					hitEnter()
-					print()
-
+				displayInformation(res, PROGRAM_WIDTH, rottenTomatoes=ROTTEN_TOMATOES, extraInfo=EXTRA_INFO)
+			# Search
 			else:
 				print()
-				printHeader('Error')
-				print(res['Error'])
-				print()
-
-			printDivider(PROGRAM_WIDTH, newLineAbove=False)
-
+				printHeader('Results for %s' % correctedQuery.upper())
+				results = []
+				ids = []
+				for result in res['Search']:
+					results.append('%s (%s)' % (result['Title'], result['Year']))
+					ids.append(result['imdbID'])
+				choice = getChoice(list(range(len(results))), results)
+				# Send request
+				print('\nRetrieving information from database...')
+				res = sendRequest(str(ids[choice]), API_URL, payload, id=True)
+				printDivider(PROGRAM_WIDTH)
+				# Display information
+				displayInformation(res, PROGRAM_WIDTH, rottenTomatoes=ROTTEN_TOMATOES, extraInfo=EXTRA_INFO)
 		else:
-			print('Something went wrong: Error %s.' % r.status_code)
-			printDivider(PROGRAM_WIDTH)
-			exit(1)
+			print()
+			printHeader('Error')
+			print(res['Error'])
+			print()
+		printDivider(PROGRAM_WIDTH, newLineAbove=False)
+
+def sendRequest(query, api, payload, id=False, search=False):
+	''' 
+	Send HTTP request to OMDb API
+	Return HTTP response parsed into JSON
+	'''
+	payload = payload.copy()
+	assert type(query) == str, 'Query must be string.'
+	if not search:
+		if not id:
+			payload['t'] = query
+		else:
+			payload['i'] = query
+	else:
+		assert id == False, 'Search by title only.'
+		payload['s'] = query
+	while True:
+		try:
+			r = requests.get(api, params=payload)
+			if r.status_code == 200:
+				return r.json()
+			else:
+				print('Something went wrong: Error %s.' % r.status_code)
+				exit(1)
+		except requests.exceptions.ConnectionError:
+			print('Cannot establish connection to database.\n')
+			hitEnter('retry')
+
+def displayInformation(response, width, rottenTomatoes=True, extraInfo=True):
+	''' Display movie information '''
+	title = response['Title']
+	year = response['Year']
+	information = [
+		# Basic information
+		OrderedDict([
+			('plot', response['Plot']),
+			('director', response['Director']),
+			('writer', response['Writer']),
+			('actors', response['Actors'])
+		])
+	]
+	if rottenTomatoes:
+		information.append(
+			# Rotten Tomatoes information
+			OrderedDict([
+				('consensus', response['tomatoConsensus']),
+				('tomatometer', response['tomatoMeter'] + '%'),
+				('average rating', response['tomatoRating'] + '/10'),
+				('url', response['tomatoURL'])
+			])
+		)
+	if extraInfo:
+		information.append(
+			# Extra information	
+			OrderedDict([
+				('genre', response['Genre']),	
+				('rated', response['Rated']),
+				('runtime', response['Runtime']),
+				('release date', response['Released'])
+			])
+		)
+	# Display information
+	print()
+	printHeader('%s (%s)' % (title, year))
+	for info in information:
+		for key in info.keys():
+			print(key.upper() + '\n')
+			detail = info[key]
+			if 'N/A' in detail:
+				detail = '-'
+			print(leftPad(leftAlign(detail, width-3), 3))
+			print()
+		hitEnter()
+		print()
 
 def autocorrect(phrase):
 	''' Return a string of suggested spelling for the phrase provided '''
