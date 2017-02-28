e10f66875bd92aec00e748e3ded02fe8b73d8266
Marcus Mu <chunkhang@gmail.com>
2017-03-01 00:02:05 +0800

    Set up project structure for PyPI

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+	name = 'moviehub',
+	packages = ['moviehub'],
+	install_requires = [
+		'beautifulsoup4', 
+		'requests'
+	],
+	version = '1.0.0',
+	description = 'A console application for obtaining movie information using OMDb API',
+	author = 'Marcus Mu',
+	author_email = 'chunkhang@gmail.com',
+	url = 'https://github.com/chunkhang/moviehub',
+	download_url = '', 
+	keywords = [
+		'movie', 
+		'hub', 
+		'omdb'
+	], 
+	classifiers = [
+		'Intended Audience :: End Users/Desktop',
+		'Programming Language :: Python :: 3 :: Only',
+		'Environment :: Console'
+	]
+)
